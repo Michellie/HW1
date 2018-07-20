88ab696bacb114e0082fffd46ebec296f9d64f24
Michellie <michellearthars@gmail.com>
2018-07-20 13:31:18 +1200

    Initial commit

---
+++ A1.1 - mart923.py
@@ -0,0 +1,17 @@
+
+def prime(n):
+    if (i % 2) == 0:
+        return False
+    for i in islice(n,2):
+
+
+
+
+
+def main():
+    k = input("Please enter k: ")
+    print("this is k:", k)
+
+
+
+main()
