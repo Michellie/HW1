829abf0d5c13f4020e32d56d08a7d1ec4b8d1045
Michellie <michellearthars@gmail.com>
2018-07-20 21:06:06 +1200

    output k lines with prime factorisation

--- A1.1 - mart923.py
+++ A1.1 - mart923.py
@@ -1,24 +1,29 @@
 
 
 def primeFactorisation (k):
+    factorString = ""
     for i in range(2, k+1):
         if (k % 2) == 0:
             count = 0
             while (k % i) == 0:
                 k = k / i
                 count += 1
-            factorString = ("  " + str(i) + " 2^" + str(count))
+            factorString = (str(i) + "^" + str(count))
     primeCount = 0      #keep count of prime number
     if k != 1:
         primeCount += 1
         factorString += "*" + str(int(k))
-    print(factorString)
+    return (factorString)
 
 
 def main():
     k = int(input("Please enter k: "))
-    primeFactorisation(k)
     print("  k pfe(k) ")
+    for i in range (k):
+        factorString = primeFactorisation(i + 1)
+        print(" " + str(i+ 1) + " " + factorString)
+
+
 
 
 main()
