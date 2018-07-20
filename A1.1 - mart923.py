de5ef6b2ec2d779793a838c67bf541eb7f378e93
Michellie <michellearthars@gmail.com>
2018-07-20 20:45:34 +1200

    Prime Factorisation Function
    Prints the prime factoristation of a number k

--- A1.1 - mart923.py
+++ A1.1 - mart923.py
@@ -1,17 +1,24 @@
 
-def prime(n):
-    if (i % 2) == 0:
-        return False
-    for i in islice(n,2):
 
-
-
+def primeFactorisation (k):
+    for i in range(2, k+1):
+        if (k % 2) == 0:
+            count = 0
+            while (k % i) == 0:
+                k = k / i
+                count += 1
+            factorString = ("  " + str(i) + " 2^" + str(count))
+    primeCount = 0      #keep count of prime number
+    if k != 1:
+        primeCount += 1
+        factorString += "*" + str(int(k))
+    print(factorString)
 
 
 def main():
-    k = input("Please enter k: ")
-    print("this is k:", k)
-
+    k = int(input("Please enter k: "))
+    primeFactorisation(k)
+    print("  k pfe(k) ")
 
 
 main()
