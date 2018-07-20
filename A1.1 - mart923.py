86e8b4c536dd6ad371382cd286fdc10364de9d29
Michellie <michellearthars@gmail.com>
2018-07-20 22:30:21 +1200

    Returns an array of prime factorisation strings for k lines to main function.

--- A1.1 - mart923.py
+++ A1.1 - mart923.py
@@ -1,29 +1,30 @@
 
 
 def primeFactorisation (k):
-    i = k
-    factorString = ""
-    for j in range(2, i+1):
-        if (i % 2) == 0:
-            count = 0
-            while (i % j) == 0:
-                i = i / j
-                count += 1
-            if count > 1:
-                factorString = (str(j) + "^" + str(count))   # only prints power that is greater than 1
+    outputList = []
+    for i in range(2,k):
+        factorString = ""
+        for j in range(2, i+1):
+            if (i % 2) == 0: # i is an even number
+                count = 0
+                while (i % j) == 0:
+                    i = i / j
+                    count += 1
+                if count > 1:
+                    factorString = (str(j) + "^" + str(count))   # only prints power that is greater than 1
+                else:
+                    factorString = (str(j))
+        primeList = []
+        if i != 1:
+            if i not in primeList:
+                primeList.append(i)
+                factorString += str(int(i))
             else:
-                factorString = (str(j))
+                prime = primeList.index(str(int(i)))
+                factorString += "*" + str(prime + 1)
 
-    primeList = []
-    if i != 1:
-        if i not in primeList:
-            primeList.append(i)
-            factorString += str(int(i))
-        else:
-            prime = primeList.index(str(int(i)))
-            factorString += "*" + str(prime + 1)
-
-    return (factorString)
+        outputList.append(factorString)
+    return outputList
 
 
 def main():
@@ -31,9 +32,10 @@ def main():
     print("  k pfe(k) ")
     if k > 1:
         print(" 1 1")
-        for i in range(1,k):
-            factorString = primeFactorisation(i + 1)
-            print(" " + str(i+ 1) + " " + factorString)
+        # for i in range(1,k):
+        factorList = primeFactorisation(k)
+        for i in range (len(factorList)):
+            print(" " + str(i + 2) + " " + factorList[i])
 
 
 
