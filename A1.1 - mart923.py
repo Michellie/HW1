7a9c03ad977ff5939f0d4e4773f818bb51de4714
Michellie <michellearthars@gmail.com>
2018-07-21 00:44:37 +1200

    Reformmats output index aligment

--- A1.1 - mart923.py
+++ A1.1 - mart923.py
@@ -1,29 +1,26 @@
-
-
 def primeFactorisation (k):
     outputList = []
     primeList = [2]
     for i in range(2,k + 1):
         factorString = ""
+        firstDigit = 1;
         for j in range(2, i+1):
-            if (i % 2) == 0: # i is an even number
+            if (i % j) == 0:
                 count = 0
                 while (i % j) == 0:
                     i = i / j
                     count += 1
                 if count > 1:
                     factorString = (str(j) + "^" + str(count))   # only prints power that is greater than 1
+                    firstDigit = 0
                 else:
-                    factorString = (str(j))
-        if i != 1:
-            if i not in primeList:
-                primeList += [i]
-                factorString += str(int(i))
-            else:
-                prime = primeList.index(int(i))
-                factorString += "*" + str(prime + 2 ) # 1: counting from 1, 2: number 2 is already in the list
+                    if firstDigit == 1:
+                        factorString +=  (str(j))
+                    else:
+                        factorString += "*" + (str(j))
+                    firstDigit = 0
+
         outputList.append(factorString)
-    print("a", primeList)
     return outputList
 
 
@@ -31,12 +28,13 @@ def main():
     k = int(input("Please enter k: "))
     print("  k pfe(k) ")
     if k > 1:
-        print(" 1 1")
+        print("  1 1")
         factorList = primeFactorisation(k)  # returns a list of prime factorisation strings
         for i in range (len(factorList)):
-            print(" " + str(i + 2) + " " + factorList[i])
-
-
-
-
+            if int(i) < 8:
+                print("  " + str(i + 2) + " " + factorList[i])
+            elif int(i) < 98:
+                print(" " + str(i + 2) + " " + factorList[i])
+            else:
+                print(str(i + 2) + "" + factorList[i])
 main()
