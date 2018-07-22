db24458c98eccd59acb25408d38a68ed91977477
Michellie <michellearthars@gmail.com>
2018-07-22 23:30:21 +1200

    Prints outputs only if the length of pfe representation is shorter than d(k) presentation

--- prob2.py
+++ prob2.py
@@ -3,53 +3,30 @@ import sys
 
 
 def primeFactorisation (k):
+    print("c")
     outputList = []
-    primeList = [2]
-    primeCounter = 2
-    for i in range(2,k + 1):
-        factorString = ""
+    for i in range(11, k + 1, 2):
+        count = 0
         for j in range(2, i+1):
-            if (i % j) == 0:
-                if i == j:
-                    if i not in primeList:       # identifies prime numbers
-                        primeList += [i]
-                        primeCounter += 1       # increase the number of prime number
-                        factorString += str(primeCounter)
-                        #factorString += str(i) + "     p: " + str(primeCounter) + " a "
-                    else:
-                        factorString += str(primeList.index(i)+ 2)      # 1: number 2 is already found, 2: index + 1 = number found
-                        #factorString += "   " + str(int(i)) + "     p: " + str(primeList.index(i)+ 2) + " b "
-                else:
-                    count = 0
-                    while (i % j) == 0:
-                        i = i / j
-                        count += 1
-                    if j not in primeList:
-                        stringValue = str(j)
-                    else:
-                        stringValue = str(primeList.index(j)+ 2)
-                    factorString += stringValue
-                    if count > 1:
-                        factorString += "^" + str(count)  # only prints power that is greater than 1
-                    if i > 1:
-                        factorString += "*"
-        outputList.append(factorString)
+            if (i % j) == 0 and i != j:
+                count += 1
+        if count == 0:
+            outputList.append(i)
     return outputList
 
 
 def main():
-    k = int(sys.stdin.readline())   # problem
+    k = int(sys.stdin.readline())
     print("  k pfe(k) ")
-    print("  1 1")
+    if k == 1:
+        print("  1 1")
     if k > 1:
         factorList = primeFactorisation(k)  # returns a list of prime factorisation strings
-        for i in range (len(factorList)):
-            if int(i) < 8:
-                print("  " + str(i + 2) + " " + factorList[i])
-            elif int(i) < 98:
-                print(" " + str(i + 2) + " " + factorList[i])
-            else:
-                print(str(i + 2) + " " + factorList[i])
+        count = 6
+        for i in factorList:
+            if len(str(count)) < len(str(i)):       # only prints strings with pfe string shorter than length of d(k) string representation
+                print(i, count)
+            count += 1
 
 
 main()
