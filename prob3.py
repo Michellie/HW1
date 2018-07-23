c9e04d35053a8573e2f8ce209c8e81785b4c4fba
Michellie <michellearthars@gmail.com>
2018-07-23 22:06:22 +1200

    Produces output in ascending exponential follows by sorted multiplication order

--- prob3.py
+++ prob3.py
@@ -3,32 +3,42 @@ import sys
 
 
 def main():
-    inputList = (sys.stdin.readline()).split()  # problem
+    input = (sys.stdin.readline()).replace("*", " ")  # replace * with space in order to split numbers from mulplication
+    inputList = input.split()
     baseDict = dict()
     if "0" in inputList:
         print("0")
     else:
         one = 0
+        power = 0
+        key = 0
         for i in inputList:
-            if "^" not in i or "*" not in i:            # case for numbers only
+            if "^" not in i and "*" not in i:            # case for numbers only
                 key = int(i)
                 if key == 1:
                     one = 1
-                elif key not in baseDict and key != 1:             # if key doesn't exist, adds key
-                    baseDict[key] = 1
-                else:                                   # if base exist, adds power into string of values
-                    baseDict[key] = baseDict[key] + 1
-        output = ""
+                power = 1
+            else:
+                basePos = i.find("^")
+                key = int(i[:basePos])
+                power = int(i[(basePos+1):])
+            exponent = ""
+            multiplication = ""
+            # print("Key:", key, "Power", power)
+            if key not in baseDict:             # if key doesn't exist, adds key
+                baseDict[key] = power
+            else:
+                baseDict[key] += power
         if one == 1 and len(baseDict) < 1:
             print("1")
         else:
             for key in sorted(baseDict):                # sort base values into ascending order
-                print("Key:", key, "Values:", baseDict[key])
-                if baseDict[key] == 1:
-                    output += (str(key) + "*")
+                print("Keya:", key, "Valuas:", baseDict[key])
+                if baseDict[key] > 1:
+                    exponent += (str(key) + "^" + str(baseDict[key]) + "*")
                 else:
-                    output += (str(key) + "^" + str(baseDict[key]) + "*")
-            print(output[:-1])      # to remove the last *
+                    multiplication += (str(key) + "*")
+            print(exponent[:-1] + "*" + multiplication[:-1])      # to remove the last *
 
 
 main()
